cdafe878cb77d1ae95230c5d91846c0b9ec0c55d
Abdulmelik Derinkök <abdulmelik.derinkok@bilecik.edu.tr>
2025-03-05 12:59:46 +0300

    u1-wtk

---
+++ bahar_24_25/web_tasarimi_ve_kodlama/u1/sayfa.php
@@ -0,0 +1,45 @@
+<?php
+$ad = "Abdulmelik";
+$soyad = "Derinkök";
+$Ad = "Test";
+$isim ="Sayın ";
+$isim .= $ad." ".$soyad;
+// $isim = $isim . $ad." ".$soyad;
+echo "Merhaba ".$isim." hoşgeldiniz.";
+echo "<br>";
+echo "Merhaba ".$ad." ".$soyad." hoşgeldiniz.";
+echo "<br>";
+
+$sayi1= 10;
+$sayi2= 3;
+$sayi3= 25.5;
+$sonuc = 0;
+echo "<br>";
+var_dump($sayi1);
+echo "<br>";
+echo "<br>";
+var_dump($sayi3);
+echo "<br>";
+echo "<br>";
+var_dump($isim);
+echo "<br>";
+$sonuc = $sayi1+$sayi2;
+
+$sonuc += $sayi3;
+// $sonuc = $sonuc + $sayi3;
+echo $sonuc;
+//Yorum
+
+
+#Yorum
+
+
+/*
+ Çoklu yorum satırı
+
+
+ */
+
+
+
+print "test";
---
+++ bahar_24_25/web_tasarimi_ve_kodlama/u1/sayfa2.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>PHP Temelleri</title>
+</head>
+<body>
+<p><?php echo "Merhaba dünya"; ?></p>
+
+
+<p>Yaşınız:<?php echo 22; ?></p>
+</body>
+</html>
