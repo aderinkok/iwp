4963de5eec5e1308e8edee6500bb03b46799f86d
Abdulmelik Derinkök <abdulmelik.derinkok@bilecik.edu.tr>
2025-03-06 16:03:17 +0300

    u1-bp

---
+++ bahar_24_25/bilgisayar_programciligi/u1/sayfa.php
@@ -0,0 +1,61 @@
+<?php
+
+$ad = "Abdul";
+$ad .= "melik";
+$soyad = "Derinkök";
+$metin= "Metin değişkeni";
+echo "<br>";
+var_dump($metin);
+echo "<br>";
+$metin = 20;
+echo "<br>";
+var_dump($metin);
+echo "<br>";
+$isim = $ad." ".$soyad;
+echo $isim;
+echo "<br>";
+echo "Merhaba sayın ".$isim." hoşgeldiniz.";
+echo "<br>";
+echo 5;
+echo "<br>";
+$sayi1 = 10;
+$sayi2 = 3;
+$sayi3 = 20;
+$sonuc = 0;
+echo "<br>";
+var_dump($sayi2);
+echo "<br>";
+
+
+echo $sayi1%$sayi2;
+echo "<br>";
+$sonuc = $sayi1**$sayi2;
+echo $sonuc;
+echo "<br>";
+$sonuc= $sayi1+$sayi2*$sayi3;
+
+$sonuc += $sayi2;
+//$sonuc = $sonuc + $sayi2;
+echo "<br>";
+echo $sonuc;
+echo "<br>";
+echo $sayi1;
+echo "<br>";
+print "Metin";
+// Yorum satırı
+
+# Yorum satırı
+
+/*
+
+Yorum satırı
+
+*/
+
+
+
+
+
+
+
+?>
---
+++ bahar_24_25/bilgisayar_programciligi/u1/sayfa2.php
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body>
+<h1><?php echo "Merhaba PHP"; ?></h1>
+
+<p><?php  echo "PHP"; ?></p>
+</body>
+</html>
