f55742a20e8a43eb7daf3f735591f4f80fd59bb5
Abdulmelik Derinkök <abdulmelik.derinkok@bilecik.edu.tr>
2025-05-22 15:49:41 +0300

    u9-bp

---
+++ bahar_24_25/bilgisayar_programciligi/u9/db.php
@@ -0,0 +1,134 @@
+<?php
+
+$host = "localhost";
+$dbName = "phpbilgisayar";
+$user = "root";
+$pass = "";
+$mesaj = "";
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbName", $user, $pass);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $mesaj = "Veritabanı bağlatısı başarılı<br>";
+}
+catch(PDOException $e){
+    $mesaj = "Veritabanı bağlantı hatası<br> ";
+}
+
+if(isset($_POST["ad"])){
+    try {
+        $sql = "INSERT INTO kullanici (ad,soyad,mail,parola,telefon) VALUES (:ad,:soyad,:mail,:parola,:telefon)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['ad' => $_POST["ad"], 'soyad' => $_POST["soyad"], 'mail' => $_POST["mail"], 'parola' => $_POST["parola"], 'telefon' => $_POST["telefon"]]);
+        $mesaj = $_POST["ad"]." ".$_POST["soyad"]."kullanıcısı başarıyla eklendi";
+    }
+    catch(PDOException $e){
+        $mesaj = "Veri ekleme hatası. ";
+    }
+    }
+
+try{
+$selectSql = "SELECT * FROM kullanici";
+$veriler = $pdo->prepare($selectSql);
+$veriler = $pdo->query($selectSql);
+$kullanicilar = $veriler->fetchAll(PDO::FETCH_ASSOC);
+}catch(PDOException $e){
+    $mesaj = "Veri okuma hatası. ";
+}
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Veritabanı</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-bg-dark">
+            <h1>Php ile veritabanı işlemleri <small>(PDO ile)</small></h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+
+            <p class="alert alert-info"><?php echo $mesaj;     ?></p>
+        </div>
+        <div class="col-md-4">
+
+
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">ad</th>
+                    <th scope="col">soyad</th>
+                    <th scope="col">#</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($kullanicilar as $kulanici) {
+                                    ?>
+                <tr>
+
+                    <td><?php echo $kulanici['id']; ?></td>
+                    <td><?php echo $kulanici['ad']; ?></td>
+                    <td><?php echo $kulanici['soyad']; ?></td>
+                    <td><a href="kullanici.php?id=<?php echo $kulanici['id']; ?>">Görüntüle</a></td>
+                </tr>
+                <?php } ?>
+
+                </tbody>
+            </table>
+
+
+        </div>
+        <div class="col-md-8 ">
+            <form action="db.php" method="POST" class="form-horizontal">
+                <div class="form-group">
+                    <label for="ad" class="col-sm-3 col-form-label">Ad</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="ad" name="ad" placeholder="Ad" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="soyad" class="col-sm-3 col-form-label">Soyad</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="soyad" name="soyad" placeholder="Soyad" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="mail" class="col-sm-3 col-form-label">Mail</label>
+                    <div class="col-sm-9">
+                        <input type="email" class="form-control" id="mail" name="mail" placeholder="mail" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="parola" class="col-sm-3 col-form-label">Parola</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="parola" name="parola" placeholder="Parola" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="telefon" class="col-sm-3 col-form-label">Telefon</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="telefon" name="telefon" placeholder="Telefon" required>
+                    </div>
+                </div>
+                <div class="form-group mt-2">
+                    <input type="submit" class="btn btn-primary" value="Gönder">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
