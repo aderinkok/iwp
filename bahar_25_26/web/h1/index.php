442f59cfa1e0e105b1abc5345006a6ede1909533
Abdulmelik Derinkök <abdulmelik93@msn.com>
2026-03-02 11:24:38 +0300

    h1

---
+++ bahar_25_26/web/h1/index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Merhaba dünya<br>";
+echo "<b>Merhaba dünya</b>";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Başlık</h1>
+
+    <p>Ders adı: <b><?php echo "İleri web programlama"; ?></b></p>
+</body>
+</html>
+
